ee53176d883d266ab348374976dd1a2666b97710
TonyMorse92 <anthonymorse92@gmail.com>
2025-05-06 18:11:05 -0400

    Putting the dealer's logic in. Have to refactor things. Checking for busts and comparing scores should be their own function. And the dealer either needs to be a subclass of a player.

--- blackjack.py
+++ blackjack.py
@@ -10,14 +10,10 @@ ranks += ['Jack','Queen','King','Ace']
 ## TODO: Deck should probably be its own class if I'm doing it like this.
 deck = [Card(suit,face) for suit in suits for face in ranks]
 
-print(f'First card: {deck[0].name}')
-
 # Shuffle 3 times. No cheating here
 random.shuffle(deck)
 random.shuffle(deck)
 random.shuffle(deck)
-
-print(f'First card after shuffling: {deck[0].name}')
 
 dealer = Player()
 me = Player()
@@ -25,12 +21,9 @@ me.draw(deck, 2)
 dealer.draw(deck,2)
 dealer.update_score()
 me.update_score()
-print(f'First score: {me.get_score()}')
 print(f'First hand: {me.get_hand()}')
-print(f'Pretend dealer hand: {dealer.get_dealer_hand()}')
-
-
-action = ''
+print(f'First score: {me.get_score()}\n')
+print(f'Dealer hand: {dealer.get_dealer_hand()}')
 
 
 # Need to check if Player has a 21. If they do, they win
@@ -49,14 +42,24 @@ while action == 'H':
 		elif me.get_score() > 21:
 			print("YOU LOSE!!!")
 			sys.exit()
-		print(f'Score: {me.get_score()}')
 		print(f'Hand: {me.get_hand()}')
-		print(f'Pretend dealer hand: {dealer.get_dealer_hand()}')
+		print(f'Dealer hand: {dealer.get_dealer_hand()}')
 
 		# Then needs to ask again
 		# If the player busts, no need to do anything with the dealer, player loses
 	elif action == 'S':
-		pass
+		if dealer.get_score() < me.get_score():
+			dealer.draw(deck,1)
+			dealer.update_score()
+			if dealer.get_score() > 21:
+				print(f'YOU WIN!!!!')
+			else:
+				print(f'PLACEHOLDER PUT IN MORE LOGIC.')
+		
+		else:
+			print(f'Dealer hand: {dealer.get_dealer_hand()}')
+			print(f'YOU LOSE!!!')
+
 		# This will be the Dealer's logic to see if they stay or hit
 		# If the Dealer would stay (so they are winning), can just reveal all the cards and say player wins
 	else:
