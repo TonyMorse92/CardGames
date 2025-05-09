42497651d6e61bde5cf33ac82d6efe202aeffbb3
TonyMorse92 <anthonymorse92@gmail.com>
2025-05-09 12:23:52 -0400

    Correcting deck permutation.

--- deck.py
+++ deck.py
@@ -1,11 +1,12 @@
 from card import Card
+import itertools
 
 class Deck:
 	def make_deck(self):
 		suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
 		ranks = [str(i) for i in range (2,11)]
 		ranks += ['Jack','Queen','King','Ace']
-		return [Card(suit, rank) for suit, rank in zip(suits, ranks)]
+		return [Card(suit, rank) for suit in suits for rank in ranks]
 
 
 deck = Deck().make_deck()
