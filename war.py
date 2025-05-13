63bb94f3280ed5567c42675ac08aebb9a7ce56b4
TonyMorse92 <anthonymorse92@gmail.com>
2025-05-13 11:53:29 -0400

    Update dealer to computer for naming.

--- war.py
+++ war.py
@@ -9,13 +9,13 @@ deck = Deck().make_deck()
 random.shuffle(deck)
 
 
-dealer = Player()
+computer = Player()
 player = Player()
 
 player.draw(deck, 2)
-dealer.draw(deck,2)
+computer.draw(deck,2)
 
-dealer.update_score()
+computer.update_score()
 player.update_score()
 
 #TODO: Eventually make this check if they want to play again.
@@ -23,7 +23,7 @@ def quit():
 	sys.exit()
 
 print(f'Hand: {player.get_hand()}')
-print(f'Dealer hand: {dealer.get_hand(hide_hand=True)}')
+print(f'Computer hand: {computer.get_hand(hide_hand=True)}')
 
 
 # Begin Game Loop
@@ -33,7 +33,7 @@ while action == 'H':
 	player.draw(deck,1)
 	player.update_score() 
 	print(f'\nHand: {player.get_hand()}')
-	print(f'Dealer hand: {dealer.get_hand(hide_hand=True)}')
+	print(f'Computer hand: {computer.get_hand(hide_hand=True)}')
 
 	if player.get_score() > 21:
 		print('Bust. You lose!')
@@ -45,29 +45,29 @@ while action == 'H':
 		action = input("\nDo you want to [H]it or [S]tay?\n")
 
 
-# Player has finished drawing. Dealer's turn. I think that's how Blackjack works? 
-# At this point, the Player hasn't gone over 21, and the dealer can't have either
-# Ties go to the player, I think. Can separate that out later, for now, just have dealer
+# Player has finished drawing. Computer's turn. I think that's how Blackjack works? 
+# At this point, the Player hasn't gone over 21, and the computer can't have either
+# Ties go to the player, I think. Can separate that out later, for now, just have computer
 # draw again and bust
-while dealer.get_score() <= player.get_score():
+while computer.get_score() <= player.get_score():
 	print(f'\nHand: {player.get_hand()}')
-	print(f'Dealer hand: {dealer.get_hand(hide_hand=False)}\n')	
-	dealer.draw(deck,1)
-	dealer.update_score()
+	print(f'Computer hand: {computer.get_hand(hide_hand=False)}\n')	
+	computer.draw(deck,1)
+	computer.update_score()
 
 	# Give the player some time to process what happened.
 	# Parameter is number of seconds to wait.
 	time.sleep(1.5)
 
-	if dealer.get_score() > 21:
+	if computer.get_score() > 21:
 		print(f'\nHand: {player.get_hand()}')
-		print(f'Dealer hand: {dealer.get_hand(hide_hand=False)}\n')
-		print(f'Dealer bust. You win!')	
+		print(f'Computer hand: {computer.get_hand(hide_hand=False)}\n')
+		print(f'Computer bust. You win!')	
 		quit()
 
 
-# Only way we haven't exited yet is if dealer has higher hand and didn't bust
+# Only way we haven't exited yet is if computer has higher hand and didn't bust
 print(f'\nHand: {player.get_hand()}')
-print(f'Dealer hand: {dealer.get_hand(hide_hand=False)}\n')
-print(f'Dealer higher. You lose!')	
+print(f'Computer hand: {computer.get_hand(hide_hand=False)}\n')
+print(f'Computer higher. You lose!')	
 quit()
